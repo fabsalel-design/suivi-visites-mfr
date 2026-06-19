96669dcc3a24394f1ae821de942e76abe6c65f8e
fabsalel-design <fabsalel@gmail.com>
2026-06-19 14:16:34 +0200

    Create page.tsx

---
+++ app/visites/page.tsx
@@ -0,0 +1,38 @@
+
+export default function VisitesPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Nouvelle visite</h1>
+
+      <label>Date de visite</label>
+      <br />
+      <input type="date" />
+
+      <br />
+      <br />
+
+      <label>Observations</label>
+      <br />
+      <textarea rows={5} cols={60} />
+
+      <br />
+      <br />
+
+      <label>Points forts</label>
+      <br />
+      <textarea rows={4} cols={60} />
+
+      <br />
+      <br />
+
+      <label>Axes d'amélioration</label>
+      <br />
+      <textarea rows={4} cols={60} />
+
+      <br />
+      <br />
+
+      <button>Passer aux signatures</button>
+    </main>
+  );
+}
