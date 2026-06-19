1293a582043d1c8861ba1fdded0e800f27bc52b3
fabsalel-design <fabsalel@gmail.com>
2026-06-19 14:03:49 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+
+export default function HomePage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Suivi des visites MFR</h1>
+
+      <p>
+        Application de suivi des visites entreprises
+      </p>
+
+      <hr />
+
+      <h2>Menu</h2>
+
+      <ul>
+        <li>Dashboard Coordinateur</li>
+        <li>Mes Apprentis</li>
+        <li>Import Excel</li>
+        <li>Documents PDF</li>
+      </ul>
+    </main>
+  );
+}
+``
