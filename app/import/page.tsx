33365924ded73a522af0a63d3af609037787aa2f
fabsalel-design <fabsalel@gmail.com>
2026-06-19 23:15:17 +0200

    Update page.tsx

--- app/import/page.tsx
+++ app/import/page.tsx
@@ -1,20 +1,34 @@
+
+"use client";
+
+import { useState } from "react";
+
 export default function ImportPage() {
+  const [message, setMessage] = useState("");
+
+  async function importer() {
+    const response = await fetch("/api/import", {
+      method: "POST",
+    });
+
+    const result = await response.json();
+
+    setMessage(result.message);
+  }
+
   return (
     <main style={{ padding: "40px" }}>
       <h1>Import Excel</h1>
 
       <p>
-        Import du fichier semestriel de visites
+        Import du fichier semestriel
       </p>
 
-      <input type="file" />
-
-      <br />
-      <br />
-
-      <button>
-        Importer
+      <button onClick={importer}>
+        Tester l'import
       </button>
+
+      <p>{message}</p>
     </main>
   );
 }
