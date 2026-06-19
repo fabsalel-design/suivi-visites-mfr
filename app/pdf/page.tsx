5807ba2318ae66b701f8ac13d6a8a4589177103a
fabsalel-design <fabsalel@gmail.com>
2026-06-19 14:20:17 +0200

    Create page.tsx

---
+++ app/pdf/page.tsx
@@ -0,0 +1,16 @@
+
+export default function PdfPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Visite enregistrée</h1>
+
+      <p>✅ Signature tuteur enregistrée</p>
+
+      <p>✅ Signature formateur enregistrée</p>
+
+      <p>✅ PDF généré</p>
+
+      <button>Télécharger le PDF</button>
+    </main>
+  );
+}
