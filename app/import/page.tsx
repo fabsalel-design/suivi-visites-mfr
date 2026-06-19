1d124b4cfa4596dd6a0eb6f179fc440b4e274dac
fabsalel-design <fabsalel@gmail.com>
2026-06-19 15:07:05 +0200

    Create page.tsx

---
+++ app/import/page.tsx
@@ -0,0 +1,20 @@
+export default function ImportPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Import Excel</h1>
+
+      <p>
+        Import du fichier semestriel de visites
+      </p>
+
+      <input type="file" />
+
+      <br />
+      <br />
+
+      <button>
+        Importer
+      </button>
+    </main>
+  );
+}
