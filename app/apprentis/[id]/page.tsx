03616d6d8cc7858d1bfc49426f1df4e5a1c2bc34
fabsalel-design <fabsalel@gmail.com>
2026-06-19 16:31:02 +0200

    Create page.tsx

---
+++ app/apprentis/[id]/page.tsx
@@ -0,0 +1,35 @@
+
+export default function FicheApprenti() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Fiche apprenti</h1>
+
+      <h2>Mateo ALARCO</h2>
+
+      <p>Formation : BTS</p>
+
+      <hr />
+
+      <h3>Entreprise</h3>
+
+      <p>SAS RAPIERE BRICOMARCHE</p>
+
+      <p>Tuteur : Christine BRILLANT</p>
+
+      <p>Téléphone : 04 90 78 23 25</p>
+
+      <hr />
+
+      <h3>Historique</h3>
+
+      <ul>
+        <li>S2 2026 - À faire</li>
+        <li>S1 2026 - PDF disponible</li>
+      </ul>
+
+      <button>
+        Réaliser la visite
+      </button>
+    </main>
+  );
+}
