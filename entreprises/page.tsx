b0b19117242d09d08f8f12d1ffdaae99e2486227
fabsalel-design <fabsalel@gmail.com>
2026-06-19 22:47:25 +0200

    Create page.tsx

---
+++ entreprises/page.tsx
@@ -0,0 +1,58 @@
+
+import { apprentis } from "../../data/apprentis";
+
+export default function EntreprisesPage() {
+  const entreprisesMap = new Map();
+
+  apprentis.forEach((apprenti) => {
+    const entreprise = apprenti.entreprise;
+
+    if (!entreprisesMap.has(entreprise)) {
+      entreprisesMap.set(entreprise, []);
+    }
+
+    entreprisesMap.get(entreprise).push(apprenti);
+  });
+
+  const entreprises = Array.from(
+    entreprisesMap.entries()
+  );
+
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Entreprises</h1>
+
+      <p>
+        Nombre d'entreprises : {entreprises.length}
+      </p>
+
+      <hr />
+
+      {entreprises.map(([nom, liste]) => (
+        <div
+          key={nom}
+          style={{
+            border: "1px solid #ddd",
+            padding: "15px",
+            marginBottom: "10px",
+            borderRadius: "8px",
+          }}
+        >
+          <h2>{nom}</h2>
+
+          <p>
+            Apprentis : {liste.length}
+          </p>
+
+          <ul>
+            {liste.map((apprenti: any) => (
+              <li key={apprenti.id}>
+                {apprenti.prenom} {apprenti.nom}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </main>
+  );
+}
