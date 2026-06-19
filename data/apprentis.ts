d534c66478d998bf422615fb891545e35a74ed1d
fabsalel-design <fabsalel@gmail.com>
2026-06-19 15:15:08 +0200

    Update apprentis.ts

--- data/apprentis.ts
+++ data/apprentis.ts
@@ -1,26 +1,6 @@
-export const apprentis = [
-  {
-    id: 1,
-    nom: "ALARCO",
-    prenom: "Mateo",
-    entreprise: "SAS RAPIERE BRICOMARCHE",
-    formateur: "FABRICE",
-    statut: "A faire",
-  },
-  {
-    id: 2,
-    nom: "AUSSEL",
-    prenom: "Nathan",
-    entreprise: "MAISON OLIVIER",
-    formateur: "FABRICE",
-    statut: "A faire",
-  },
-  {
-    id: 3,
-    nom: "BODIN",
-    prenom: "Mathieu",
-    entreprise: "TRUFFAUT",
-    formateur: "CHRISTINE",
-    statut: "A faire",
-  }
-];
+import { apprentis } from "../../data/apprentis";
+const resultat = apprentis.filter((a) =>
+  `${a.nom} ${a.prenom}`
+    .toLowerCase()
+    .includes(recherche.toLowerCase())
+
