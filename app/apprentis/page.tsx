75947816ca4350f2e3d84af7f0405e5892494614
fabsalel-design <fabsalel@gmail.com>
2026-06-19 21:49:58 +0200

    Update page.tsx

--- app/apprentis/page.tsx
+++ app/apprentis/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { useState } from "react";
@@ -6,16 +5,27 @@ import { apprentis } from "../../data/apprentis";
 
 export default function ApprentisPage() {
   const [recherche, setRecherche] = useState("");
+  const [formateur, setFormateur] = useState("TOUS");
 
-  const resultat = apprentis.filter((a) =>
-    `${a.nom} ${a.prenom}`
+  const resultat = apprentis.filter((a) => {
+    const matchNom = `${a.nom} ${a.prenom}`
       .toLowerCase()
-      .includes(recherche.toLowerCase())
-  );
+      .includes(recherche.toLowerCase());
+
+    const matchFormateur =
+      formateur === "TOUS" ||
+      a.formateur === formateur;
+
+    return matchNom && matchFormateur;
+  });
 
   return (
     <main style={{ padding: "40px" }}>
       <h1>Liste des apprentis</h1>
+
+      <p>
+        Nombre d'apprentis : {resultat.length}
+      </p>
 
       <input
         type="text"
@@ -25,9 +35,23 @@ export default function ApprentisPage() {
         style={{
           padding: "10px",
           width: "300px",
-          marginBottom: "20px",
+          marginRight: "20px",
         }}
       />
+
+      <select
+        value={formateur}
+        onChange={(e) =>
+          setFormateur(e.target.value)
+        }
+      >
+        <option value="TOUS">Tous</option>
+        <option value="FABRICE">FABRICE</option>
+        <option value="DAVID">DAVID</option>
+        <option value="CHRISTINE">CHRISTINE</option>
+      </select>
+
+      <hr />
 
       {resultat.map((apprenti) => (
         <div
@@ -44,23 +68,25 @@ export default function ApprentisPage() {
           </h3>
 
           <p>
-            <strong>Entreprise :</strong> {apprenti.entreprise}
+            <strong>Entreprise :</strong>{" "}
+            {apprenti.entreprise}
           </p>
 
           <p>
-            <strong>Formateur :</strong> {apprenti.formateur}
+            <strong>Formateur :</strong>{" "}
+            {apprenti.formateur}
           </p>
 
           <p>
-            <strong>Statut :</strong> {apprenti.statut}
+            <strong>Statut :</strong>{" "}
+            {apprenti.statut}
           </p>
 
-          
-<a href="/apprentis/1">
-  <button>
-    Ouvrir la fiche
-  </button>
-</a>
+          <a href={`/apprentis/${apprenti.id}`}>
+            <button>
+              Ouvrir la fiche
+            </button>
+          </a>
         </div>
       ))}
     </main>
