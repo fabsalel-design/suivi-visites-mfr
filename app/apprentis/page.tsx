62f7b0bdb8fac7b6fa2befa38671decaf2e5b844
fabsalel-design <fabsalel@gmail.com>
2026-06-20 15:25:39 +0200

    Update page.tsx

--- app/apprentis/page.tsx
+++ app/apprentis/page.tsx
@@ -1,59 +1,33 @@
-"use client";
 
-import { useState } from "react";
-import { apprentis } from "../../data/apprentis";
+import { supabase } from "../../lib/supabase";
 
-export default function ApprentisPage() {
-  const [recherche, setRecherche] = useState("");
-  const [formateur, setFormateur] = useState("TOUS");
+export const dynamic = "force-dynamic";
 
-  const resultat = apprentis.filter((a) => {
-    const matchNom = `${a.nom} ${a.prenom}`
-      .toLowerCase()
-      .includes(recherche.toLowerCase());
-
-    const matchFormateur =
-      formateur === "TOUS" ||
-      a.formateur === formateur;
-
-    return matchNom && matchFormateur;
-  });
+export default async function ApprentisPage() {
+  const { data: apprentis, error } = await supabase
+    .from("apprentis")
+    .select("*")
+    .order("nom");
 
   return (
     <main style={{ padding: "40px" }}>
       <h1>Liste des apprentis</h1>
 
+      {error && (
+        <p>
+          Erreur : {error.message}
+        </p>
+      )}
+
       <p>
-        Nombre d'apprentis : {resultat.length}
+        Nombre d'apprentis :
+        {" "}
+        {apprentis?.length || 0}
       </p>
-
-      <input
-        type="text"
-        placeholder="Rechercher un apprenti"
-        value={recherche}
-        onChange={(e) => setRecherche(e.target.value)}
-        style={{
-          padding: "10px",
-          width: "300px",
-          marginRight: "20px",
-        }}
-      />
-
-      <select
-        value={formateur}
-        onChange={(e) =>
-          setFormateur(e.target.value)
-        }
-      >
-        <option value="TOUS">Tous</option>
-        <option value="FABRICE">FABRICE</option>
-        <option value="DAVID">DAVID</option>
-        <option value="CHRISTINE">CHRISTINE</option>
-      </select>
 
       <hr />
 
-      {resultat.map((apprenti) => (
+      {apprentis?.map((apprenti) => (
         <div
           key={apprenti.id}
           style={{
@@ -64,29 +38,26 @@ export default function ApprentisPage() {
           }}
         >
           <h3>
-            {apprenti.nom} {apprenti.prenom}
+            {apprenti.prenom} {apprenti.nom}
           </h3>
 
           <p>
-            <strong>Entreprise :</strong>{" "}
+            Entreprise :
+            {" "}
             {apprenti.entreprise}
           </p>
 
           <p>
-            <strong>Formateur :</strong>{" "}
+            Formateur :
+            {" "}
             {apprenti.formateur}
           </p>
 
           <p>
-            <strong>Statut :</strong>{" "}
+            Statut :
+            {" "}
             {apprenti.statut}
           </p>
-
-          <a href={`/apprentis/${apprenti.id}`}>
-            <button>
-              Ouvrir la fiche
-            </button>
-          </a>
         </div>
       ))}
     </main>
