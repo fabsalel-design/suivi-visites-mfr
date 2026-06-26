3f026f3b4ff780ec506daff4c6bcd5c7ecf76889
fabsalel-design <fabsalel@gmail.com>
2026-06-26 23:54:22 +0200

    Update generatePeriodeEssaiPdf.ts

--- lib/generatePeriodeEssaiPdf.ts
+++ lib/generatePeriodeEssaiPdf.ts
@@ -19,6 +19,25 @@ export async function generatePeriodeEssaiPdf() {
   const page =
     pdfDoc.getPage(0);
 
+const X_NON_ACQUISE = 275;
+const X_EN_COURS = 410;
+const X_ACQUISE = 550;
+  
+const Y_DYNAMISME = 502;
+const Y_ESPRIT_INITIATIVE = 472;
+const Y_SENS_ORGANISATION = 437;  
+const Y_INTERET_MOTIVATION = 537;
+const Y_DYNAMISME = 502;
+const Y_ESPRIT_INITIATIVE = 472;
+const Y_SENS_ORGANISATION = 437;
+const Y_VOLONTE_CHANGEMENT = 402;
+const Y_RELATIONS_EQUIPE = 367;
+const Y_ADAPTATION = 332;
+const Y_PRESENTATION = 297;
+const Y_COMPREHENSION_CONSIGNES = 262;
+const Y_APPLICATION_REGLES = 227;
+const Y_APTITUDES_PHYSIQUES = 192;
+
 page.drawText("DATE TEST", {
   x: 150,
   y: 642,
