6ee1a3f5023abe17f98bdb13f3d291ba6f008550
fabsalel-design <fabsalel@gmail.com>
2026-07-01 13:51:38 +0200

    Update generateFinFormationPdf.ts

--- lib/generateFinFormationPdf.ts
+++ lib/generateFinFormationPdf.ts
@@ -35,8 +35,8 @@ const Y_RESPONSABILITES = 501;
 const Y_JUGEMENT = 458;
 const Y_COMMUNICATION = 420;
 const Y_RELATIONS = 387;
-const Y_ADAPTATION = 330;
-const Y_TRAVAIL_BIEN_FAIT = 285;
+const Y_ADAPTATION = 340;
+const Y_TRAVAIL_BIEN_FAIT = 295;
 
 
 function drawCroix(
