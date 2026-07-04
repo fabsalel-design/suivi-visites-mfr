3b3682f99c510dd5f67e3f9fb56d5dadb0de5c01
fabsalel-design <fabsalel@gmail.com>
2026-07-04 17:48:01 +0200

    Update FormateurMap.tsx

--- components/FormateurMap.tsx
+++ components/FormateurMap.tsx
@@ -10,6 +10,19 @@ import {
 
 import "leaflet/dist/leaflet.css";
 
+import L from "leaflet";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
 type Etablissement = {
   entreprise: string;
   ville: string;
