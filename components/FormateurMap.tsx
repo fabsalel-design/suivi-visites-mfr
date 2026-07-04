6c3a22a358d3c23db3c0528b00e1426e33a02998
fabsalel-design <fabsalel@gmail.com>
2026-07-04 23:25:09 +0200

    Update FormateurMap.tsx

--- components/FormateurMap.tsx
+++ components/FormateurMap.tsx
@@ -207,25 +207,25 @@ export default function FormateurMap({
         </div>
       )
     )}
-
-    <hr />
-
-   
+    
 <hr />
 
-<a
-  href={`https://maps.googleencodeURIComponent("
+https://maps.google.com/?q=${encodeURIComponent(
+    `${e.adresse || ""} ${e.cp || ""} ${e.ville}`
+  )}`}
+  target="_blank"
   rel="noreferrer"
   style={{
     color: "#005CA9",
     fontWeight: "bold",
     textDecoration: "none",
+
   }}
 >
   📍 Ouvrir dans Google Maps
 </a>
 
-  </div>
+</div>
 </Popup>
 
 
