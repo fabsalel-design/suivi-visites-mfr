622311c8153fa49ad30758e76867b770ed3b59b1
fabsalel-design <fabsalel@gmail.com>
2026-07-04 16:55:27 +0200

    Update FormateurMap.tsx

--- components/FormateurMap.tsx
+++ components/FormateurMap.tsx
@@ -1,21 +1,46 @@
 
 "use client";
 
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+} from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+
 export default function FormateurMap() {
   return (
-    <div
+    <MapContainer
+      center={[43.8367, 4.3601]}
+      zoom={8}
       style={{
-        height: "400px",
-        background: "#f5f7fa",
-        border: "2px dashed #005CA9",
+        height: "500px",
+        width: "100%",
         borderRadius: "12px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontWeight: "bold",
       }}
     >
-      🗺️ Carte OpenStreetMap (à venir)
-    </div>
+      <TileLayer
+        attribution="&copy; OpenStreetMap"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <Marker
+        position={[43.8367, 4.3601]}
+      >
+        <Popup>
+          Nîmes
+        </Popup>
+      </Marker>
+
+      <Marker
+        position={[43.6766, 4.6278]}
+      >
+        <Popup>
+          Arles
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
