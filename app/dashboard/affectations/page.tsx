8ca4712d2ee6856f44e738f813bbae78d0207932
fabsalel-design <fabsalel@gmail.com>
2026-07-07 00:46:37 +0200

    Update page.tsx

--- app/dashboard/affectations/page.tsx
+++ app/dashboard/affectations/page.tsx
@@ -222,20 +222,21 @@ export default async function AffectationsPage() {
                       "⚠️ Non affecté"}
                   </p>
 
-<button
-  style={{
+<Link
+hboard/affectations/${encodeURIComponent(={{
+    display: "inline-block",
     backgroundColor: "#005CA9",
     color: "white",
-    border: "none",
     padding: "8px 12px",
     borderRadius: "8px",
+    textDecoration: "none",
     fontWeight: "bold",
-    cursor: "pointer",
     marginBottom: "15px",
   }}
 >
   ✏️ Affecter
-</button>
+</Link>
+
 
                   <ul>
                     {apprenants.map(
