c2178c4400c6c9329abac947df02fa89653a9824
fabsalel-design <fabsalel@gmail.com>
2026-07-08 23:29:18 +0200

    Update page.tsx

--- app/dashboard/affectations/page.tsx
+++ app/dashboard/affectations/page.tsx
@@ -319,24 +319,19 @@ const nbNonAffectes =
                       "⚠️ Non affecté"}
                   </p>
 
-<button
-  style={{
-    backgroundColor: "#005CA9",
-    color: "white",
-    border: "none",
-    padding: "8px 12px",
-    borderRadius: "8px",
-    fontWeight: "bold",
-    cursor: "pointer",
-    marginBottom: "15px",
-  }}
+{`/dashboard/affectations/${encodeURIComponent(={{
+display: "inline-block",
+backgroundColor: "#005CA9",
+color: "white",
+padding: "8px 12px",
+borderRadius: "8px",
+fontWeight: "bold",
+textDecoration: "none",
+marginBottom: "15px",
+}}
 >
-  ✏️ Affecter
-</button>
-
-
-
-
+✏️ Affecter
+</Link>
 
 
                   <ul>
