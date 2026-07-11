c4482bfc544a9245355bc77d485fc8969e3e17ea
fabsalel-design <fabsalel@gmail.com>
2026-07-11 13:29:03 +0200

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -421,12 +421,24 @@ display: "inline-block",
 
        
 {formateurs.map((formateur) => {
-  const count =
-    apprentis?.filter(
-      (a) =>
-        a.formateur === formateur
-    ).length || 0;
-
+ const total =
+apprentis?.filter(
+(a) => a.formateur === formateur
+).length || 0;
+ 
+const realisees =
+apprentis?.filter(
+(a) =>
+a.formateur === formateur &&
+a.statut === "Terminée"
+).length || 0;
+ 
+const pourcentage =
+total > 0
+? Math.round(
+(realisees / total) * 100
+)
+: 0;
   const maxCount = Math.max(
     ...(formateurs.map(
       (f) =>
